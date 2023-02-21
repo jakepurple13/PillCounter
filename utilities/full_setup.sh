860273140d2f4b35f7b0212cb1e623a824cf8659
Jacob Rein <jakerein@gmail.com>
2023-02-21 13:17:41 -0500

    Hopefully the full_setup.sh will work flawlessly

--- utilities/full_setup.sh
+++ utilities/full_setup.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
 
+#Step 1
 curl https://raw.githubusercontent.com/jakepurple13/PillCounter/main/utilities/hx711_example.py >> ~/Desktop/hx711_example.py
 curl https://raw.githubusercontent.com/jakepurple13/PillCounter/main/utilities/emulated_hx711.py >> ~/Desktop/emulated_hx711.py
 curl https://raw.githubusercontent.com/jakepurple13/PillCounter/main/utilities/hx711.py >> ~/Desktop/hx711.py
+
+#Step 2
 curl https://raw.githubusercontent.com/jakepurple13/PillCounter/main/utilities/pillcounter.desktop >> ~/Desktop/pillcounter.desktop
 sudo mv ~/Desktop/pillcounter.desktop /etc/xdg/autostart/pillcounter.desktop
 
+#Step 3
+curl https://github.com/jakepurple13/PillCounter/releases/download/v0.0.1/pillcounter.jar -o ~/Desktop/pillcounter.jar
+
+#Step 4
 sudo apt update
 sudo apt install default-jdk
-
-curl https://github.com/jakepurple13/PillCounter/releases/download/v0.0.1/pillcounter.jar -o ~/Desktop/pillcounter.jar
